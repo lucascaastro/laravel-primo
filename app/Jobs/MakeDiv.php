25e89d69cf1e5f163a92ba0c4ce80fbb86042548
lucascaastro <lucasgrrock@gmail.com>
2022-02-04 10:44:38 -0300

    Fazer Div

---
+++ app/Jobs/MakeDiv.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use App\Notifications\DivMade;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class MakeDiv implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $num1;
+    protected $num2;
+    protected $id;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($num1, $num2, $id)
+    {
+        $this->num1 = $num1;
+        $this->num2 = $num2;
+        $this->id = $id;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $user = auth()->user();
+        $div = $this->num1 / $this->num2;
+        $user = User::find($this->userId);
+        if ($this->num2 == 0) {
+            $user->notify(new DivMade(
+                'Erro',
+                'Divisão por zero'
+            ));
+        }
+
+        $user->notify(new DivMade(
+            'Sucesso',
+            'Div = ' . $div
+        ));
+    }
+}
